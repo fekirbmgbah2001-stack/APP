b015cd509a68b3bd9eadc897dbf335b95233cba8
fekirbmgbah2001-stack <fekrbmgbah2001@gmail.com>
2026-04-21 23:02:18 +0200

    j'ai ajouter une ligne de code sur le main

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main  {
     public static void main(String[] args) {
+        System.out.println("je effectuer une modiffication sur la branche main");
         JO jourdeJO = JO.RIO_2024;
         System.out.println(jourdeJO);
     }
